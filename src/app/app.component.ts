512e10832fe3778c0d40f519601d58bfeaa565b4
akshatha283 <akshathas28302@gmail.com>
2025-04-02 15:15:13 +0530

    many changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { TurstedCompaniesComponent } from './components/tursted-companies/tursted-companies.component';
 import { OurProcessComponent } from './components/our-process/our-process.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,CarouselComponent, TurstedCompaniesComponent, OurProcessComponent],
+  imports: [HeaderComponent,CarouselComponent, TurstedCompaniesComponent, OurProcessComponent, WhyUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
